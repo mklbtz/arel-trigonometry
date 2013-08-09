c2737f582531d7949ff48b3fdd326a4528c1cdc6
Jacob Swanner <jacob@jacobswanner.com>
2013-08-09 13:45:06 -0400

    Improve spec assertions

--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -1,25 +1,26 @@
 shared_examples 'trigonometry examples' do
   let(:query) { project(function).to_sql }
   let(:result) { parse_results(execute(query)) }
+  let(:threshold) { 0.000_000_001 }
 
   context 'arcsine' do
     let(:function) { Arel::Nodes::Arcsine.new(1) }
-    specify { expect(result.to_f).to be_within(0.000_000_001).of(1.570_796_326) }
+    specify { expect(result.to_f).to be_within(threshold).of(Math.asin(1)) }
   end
 
   context 'arctangent' do
     let(:function) { Arel::Nodes::Arctangent.new(1) }
-    specify { expect(result.to_f).to be_within(0.000_000_001).of(0.785_398_163) }
+    specify { expect(result.to_f).to be_within(threshold).of(Math.atan(1)) }
   end
 
   context 'arctangent2' do
     let(:function) { Arel::Nodes::Arctangent2.new(1, 2) }
-    specify { expect(result.to_f).to be_within(0.000_000_001).of(0.463_647_609) }
+    specify { expect(result.to_f).to be_within(threshold).of(Math.atan2(1, 2)) }
   end
 
   context 'cosine' do
     let(:function) { Arel::Nodes::Cosine.new(1) }
-    specify { expect(result.to_f).to be_within(0.000_000_001).of(0.540_302_305) }
+    specify { expect(result.to_f).to be_within(threshold).of(Math.cos(1)) }
   end
 
   context 'cube root' do
@@ -33,13 +34,13 @@ shared_examples 'trigonometry examples' do
   end
 
   context 'radians' do
-    let(:function) { Arel::Nodes::Radians.new(180) }
-    specify { expect(result.to_f).to be_within(0.000_000_001).of(3.141_592_653) }
+    let(:function) { Arel::Nodes::Radians.new(1) }
+    specify { expect(result.to_f).to be_within(threshold).of(Math::PI / 180) }
   end
 
   context 'sine' do
-    let(:function) { Arel::Nodes::Sine.new(360) }
-    specify { expect(result.to_f).to be_within(0.000_000_001).of(0.958_915_723) }
+    let(:function) { Arel::Nodes::Sine.new(1) }
+    specify { expect(result.to_f).to be_within(threshold).of(Math.sin(1)) }
   end
 
   context 'square root' do
@@ -49,6 +50,6 @@ shared_examples 'trigonometry examples' do
 
   context 'tangent' do
     let(:function) { Arel::Nodes::Tangent.new(1) }
-    specify { expect(result.to_f).to be_within(0.000_000_001).of(1.557_407_724) }
+    specify { expect(result.to_f).to be_within(threshold).of(Math.tan(1)) }
   end
 end
