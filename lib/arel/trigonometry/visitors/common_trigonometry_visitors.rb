4590eece8de347e5bb291ea2f59752ac58e3bd68
Víctor Martín <ilovemayalopez@gmail.com>
2015-06-10 12:15:16 +0200

    With aggregate

--- lib/arel/trigonometry/visitors/common_trigonometry_visitors.rb
+++ lib/arel/trigonometry/visitors/common_trigonometry_visitors.rb
@@ -4,43 +4,43 @@ module Arel
       private
 
       def visit_Arel_Nodes_Arcsine o, collector
-        "ASIN(#{visit o.expr})"
+        aggregate "ASIN", o, collector
       end
 
       def visit_Arel_Nodes_Arctangent o, collector
-        "ATAN(#{visit o.expr})"
+        aggregate "ATAN", o, collector
       end
 
       def visit_Arel_Nodes_Arctangent2 o, collector
-        "ATAN2(#{visit o.left}, #{visit o.right})"
+        aggregate "ATAN", o, collector
       end
 
       def visit_Arel_Nodes_Cosine o, collector
-        "COS(#{visit o.expr})"
+        aggregate "COS", o, collector
       end
 
       def visit_Arel_Nodes_CubeRoot o, collector
-        "CBRT(#{visit o.expr})"
+        aggregate "CBRT", o, collector
       end
 
       def visit_Arel_Nodes_Pow o, collector
-        "POW(#{visit o.left}, #{visit o.right})"
+        aggregate "POW", o, collector
       end
 
       def visit_Arel_Nodes_Radians o, collector
-        "RADIANS(#{visit o.expr})"
+        aggregate "RADIANS", o, collector
       end
 
       def visit_Arel_Nodes_Sine o, collector
-        "SIN(#{visit o.expr})"
+        aggregate "SIN", o, collector
       end
 
       def visit_Arel_Nodes_SquareRoot o, collector
-        "SQRT(#{visit o.expr})"
+        aggregate "SQRT", o, collector
       end
 
       def visit_Arel_Nodes_Tangent o, collector
-        "TAN(#{visit o.expr})"
+        aggregate "TAN", o, collector
       end
     end
   end
