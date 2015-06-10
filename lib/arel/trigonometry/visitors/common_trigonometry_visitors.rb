5a1d7b49c5b1ffb546102a1c49e6ac62ca6bb115
Víctor Martín <ilovemayalopez@gmail.com>
2015-06-10 12:04:20 +0200

    Adding a collector or it will break

--- lib/arel/trigonometry/visitors/common_trigonometry_visitors.rb
+++ lib/arel/trigonometry/visitors/common_trigonometry_visitors.rb
@@ -3,43 +3,43 @@ module Arel
     module CommonTrigonometryVisitors
       private
 
-      def visit_Arel_Nodes_Arcsine o
+      def visit_Arel_Nodes_Arcsine o, collector
         "ASIN(#{visit o.expr})"
       end
 
-      def visit_Arel_Nodes_Arctangent o
+      def visit_Arel_Nodes_Arctangent o, collector
         "ATAN(#{visit o.expr})"
       end
 
-      def visit_Arel_Nodes_Arctangent2 o
+      def visit_Arel_Nodes_Arctangent2 o, collector
         "ATAN2(#{visit o.left}, #{visit o.right})"
       end
 
-      def visit_Arel_Nodes_Cosine o
+      def visit_Arel_Nodes_Cosine o, collector
         "COS(#{visit o.expr})"
       end
 
-      def visit_Arel_Nodes_CubeRoot o
+      def visit_Arel_Nodes_CubeRoot o, collector
         "CBRT(#{visit o.expr})"
       end
 
-      def visit_Arel_Nodes_Pow o
+      def visit_Arel_Nodes_Pow o, collector
         "POW(#{visit o.left}, #{visit o.right})"
       end
 
-      def visit_Arel_Nodes_Radians o
+      def visit_Arel_Nodes_Radians o, collector
         "RADIANS(#{visit o.expr})"
       end
 
-      def visit_Arel_Nodes_Sine o
+      def visit_Arel_Nodes_Sine o, collector
         "SIN(#{visit o.expr})"
       end
 
-      def visit_Arel_Nodes_SquareRoot o
+      def visit_Arel_Nodes_SquareRoot o, collector
         "SQRT(#{visit o.expr})"
       end
 
-      def visit_Arel_Nodes_Tangent o
+      def visit_Arel_Nodes_Tangent o, collector
         "TAN(#{visit o.expr})"
       end
     end
