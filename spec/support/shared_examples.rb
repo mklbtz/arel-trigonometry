746d813d861f71fc75347a8c83c8ae99a2ada63f
Michael Bates <mklbtz@gmail.com>
2016-04-19 10:37:31 -0400

    Update `new` calls to match Arel::Function.new
    Since trig function nodes now inherit from Arel::Function (1087776), they need to be initialized with an array of arguments.

--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -4,52 +4,52 @@ shared_examples 'trigonometry examples' do
   let(:threshold) { 0.000_000_001 }
 
   context 'arcsine' do
-    let(:function) { Arel::Nodes::Arcsine.new(1) }
+    let(:function) { Arel::Nodes::Arcsine.new([1]) }
     specify { expect(result.to_f).to be_within(threshold).of(Math.asin(1)) }
   end
 
   context 'arctangent' do
-    let(:function) { Arel::Nodes::Arctangent.new(1) }
+    let(:function) { Arel::Nodes::Arctangent.new([1]) }
     specify { expect(result.to_f).to be_within(threshold).of(Math.atan(1)) }
   end
 
   context 'arctangent2' do
-    let(:function) { Arel::Nodes::Arctangent2.new(1, 2) }
+    let(:function) { Arel::Nodes::Arctangent2.new([1, 2]) }
     specify { expect(result.to_f).to be_within(threshold).of(Math.atan2(1, 2)) }
   end
 
   context 'cosine' do
-    let(:function) { Arel::Nodes::Cosine.new(1) }
+    let(:function) { Arel::Nodes::Cosine.new([1]) }
     specify { expect(result.to_f).to be_within(threshold).of(Math.cos(1)) }
   end
 
   context 'cube root' do
-    let(:function) { Arel::Nodes::CubeRoot.new(16) }
+    let(:function) { Arel::Nodes::CubeRoot.new([16]) }
     specify { expect(result.to_i).to eq 2 }
   end
 
   context 'pow' do
-    let(:function) { Arel::Nodes::Pow.new(4, 2) }
+    let(:function) { Arel::Nodes::Pow.new([4, 2]) }
     specify { expect(result.to_i).to eq 16 }
   end
 
   context 'radians' do
-    let(:function) { Arel::Nodes::Radians.new(1) }
+    let(:function) { Arel::Nodes::Radians.new([1]) }
     specify { expect(result.to_f).to be_within(threshold).of(Math::PI / 180) }
   end
 
   context 'sine' do
-    let(:function) { Arel::Nodes::Sine.new(1) }
+    let(:function) { Arel::Nodes::Sine.new([1]) }
     specify { expect(result.to_f).to be_within(threshold).of(Math.sin(1)) }
   end
 
   context 'square root' do
-    let(:function) { Arel::Nodes::SquareRoot.new(16) }
+    let(:function) { Arel::Nodes::SquareRoot.new([16]) }
     specify { expect(result.to_i).to eq 4 }
   end
 
   context 'tangent' do
-    let(:function) { Arel::Nodes::Tangent.new(1) }
+    let(:function) { Arel::Nodes::Tangent.new([1]) }
     specify { expect(result.to_f).to be_within(threshold).of(Math.tan(1)) }
   end
 end
